7218785b524df29b4b4fbd6945dd6e7e834f3f25
Rob Hanssen <Rob.Hanssen@milliken.com>
2020-09-08 16:50:29 -0400

    changed source tag

--- config.r
+++ config.r
@@ -3,7 +3,7 @@
 #
 
 homesale_file = "homesalesdata-source.csv"
-source = "\U00A9 2020, Rob Hanssen\nSources: realtor.com, zillow.com, spartanburgdeeds.com"
+source = "\U00A9 2020, Rob Hanssen\nSources: realtor.com, zillow.com, spartanburgdeeds.com, other public sources"
 n_townhomes = 32
 n_patiohomes = 32
 n_residential = 484 - (n_townhomes + n_patiohomes)
