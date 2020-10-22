0394fc8d34216e0aec82ae5bb35895d7681b69f8
Rob Hanssen <Rob.Hanssen@milliken.com>
2020-10-22 13:22:48 -0400

    boxplot changed to violinplot

--- glenlake-homesales.r
+++ glenlake-homesales.r
@@ -104,11 +104,16 @@ timeonmarket %>% ggplot() + aes(x=listingyear,y=mediantimeonmarket, fill=hometyp
 write_csv(timeonmarket,"data/median-time-on-market.csv")
 ggsave("graphs/median-time-on-market.pdf")
 
-# boxplot of time on market by year and hometype
-homesales %>% ggplot() + aes(x=factor(listingyear),y=timeonmarket, fill=hometype) + geom_boxplot() + facet_wrap(.~hometype) + #geom_jitter() +
+# # boxplot of time on market by year and hometype
+# homesales %>% ggplot() + aes(x=factor(listingyear),y=timeonmarket, fill=hometype) + geom_boxplot() + facet_wrap(.~hometype) + #geom_jitter() +
+#                     xlab("Year of listing") + ylab("Time on market (in days)") + ggtitle("Distribution of time on market for sold homes in Glen Lake") + labs(fill = "Home type", caption=source) +
+#                     scale_y_continuous(limits=c(0,350))
+# ggsave("graphs/boxplot-time-on-market.pdf")
+
+homesales %>% ggplot() + aes(x=factor(listingyear),y=timeonmarket, fill=hometype) + geom_violin(draw_quantiles = 0.5) + facet_wrap(.~hometype) + #geom_jitter() +
                     xlab("Year of listing") + ylab("Time on market (in days)") + ggtitle("Distribution of time on market for sold homes in Glen Lake") + labs(fill = "Home type", caption=source) +
                     scale_y_continuous(limits=c(0,350))
-ggsave("graphs/boxplot-time-on-market.pdf")                
+ggsave("graphs/violinplot-time-on-market.pdf")
 
 
 # turn-over rate per year by hometype
