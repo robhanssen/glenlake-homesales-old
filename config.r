b4122dbd23910ae09d71d647100df556030486c7
Rob Hanssen <rob.hanssen@outlook.com>
2021-02-09 10:58:35 -0500

    year tag in copyright notice update

--- config.r
+++ config.r
@@ -2,8 +2,10 @@
 # definition of constants, etc.
 #
 
+YEAR = year(today())
+
 homesale_file = "homesalesdata-source.csv"
-source = "\U00A9 2020, Rob Hanssen\nSources: realtor.com, zillow.com, spartanburgdeeds.com, other public sources"
+source = paste0("\U00A9 ", YEAR,", Rob Hanssen\nSources: realtor.com, zillow.com, spartanburgdeeds.com, other public sources")
 n_townhomes = 32
 n_patiohomes = 32
 n_residential = 484 - (n_townhomes + n_patiohomes)
