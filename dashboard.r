67aa7944b7bceb40d3549d392ee392f2e692cae6
Rob Hanssen <rob.hanssen@outlook.com>
2021-09-27 21:14:59 -0400

    graphics updates

--- dashboard.r
+++ dashboard.r
@@ -7,83 +7,102 @@ alpha <- .5
 color <- "darkgreen"
 
 homesales <-
-    read_csv("data/homesales_processeddata.csv") #%>%
-    #filter(hometype == "residential")
+    read_csv("data/homesales_processeddata.csv") %>%
+    mutate(listingyear = factor(listingyear),
+            saleyear = factor(saleyear))
 
 max_date <- max(max(homesales$listingdate), max(homesales$saledate, na.rm = TRUE))
 max_date <- format(max_date, format = "%b %d, %Y")
+
+num_years = homesales %>% distinct(listingyear) %>% nrow(.)
+
+colorscale <- scales::seq_gradient_pal("blue", "red", "Lab")(seq(0, 1, length.out = num_years))
+
 
 homeslisted <-
     homesales %>% 
     group_by(listingyear) %>%
     summarize(count = n()) %>%
     ggplot +
-        aes(x = listingyear, y = count) +
-        geom_col(alpha = alpha, fill = color) +
+        aes(x = listingyear, y = count, fill = listingyear) +
+        geom_col(alpha = alpha) +
         labs(x = "Year",
              y = "Homes listed",
-             title = "Number of homes listed by year")
+             title = "Number of homes listed by year") + 
+        scale_fill_manual(values = colorscale) +
+        theme(legend.position = "none")
 
 homessold <-
-    homesales %>% 
+    homesales %>%
+    filter(!is.na(saleyear)) %>%
     group_by(saleyear) %>%
     summarize(count = n()) %>%
     ggplot +
-        aes(x = saleyear, y = count) +
-        geom_col(alpha = alpha, fill = color) +
+        aes(x = saleyear, y = count, fill = saleyear) +
+        geom_col(alpha = alpha) +
         labs(x = "Year",
              y = "Homes sold",
-             title = "Number of homes sold by year")
+             title = "Number of homes sold by year") +
+        scale_fill_manual(values = colorscale) +
+        theme(legend.position = "none")
 
 timeonmarket <-
     homesales %>%
-    filter(!is.na(listingyear)) %>%
+#    filter(!is.na(listingyear)) %>%
     group_by(listingyear) %>%
     summarize(timeonmarket = median(timeonmarket))  %>%
     ggplot +
-        aes(x = listingyear, y = timeonmarket) +
-        geom_col(alpha = alpha, fill = color) +
+        aes(x = listingyear, y = timeonmarket, fill = listingyear) +
+        geom_col(alpha = alpha) +
         labs(x = "Year",
              y = "Median time on market (in days)",
-             title = "Median time on market by year")
+             title = "Median time on market by year") +
+        scale_fill_manual(values = colorscale) +
+        theme(legend.position = "none")
 
 saleprice <-
     homesales %>% 
+    filter(!is.na(saleyear)) %>%
     group_by(saleyear) %>%
-    summarize(saleprice = median(amount)) %>%
+    summarize(saleprice = median(amount), na.rm = TRUE) %>%
     ggplot +
-        aes(x = saleyear, y = saleprice) +
-        geom_col(alpha = alpha, fill = color) +
+        aes(x = saleyear, y = saleprice, fill = saleyear) +
+        geom_col(alpha = alpha) +
         scale_y_continuous(labels = scales::dollar_format(scale = 1/1000, suffix = "K")) + 
         labs(x = "Year",
              y = "Median sale price (in $)",
-             title = "Median sale price by year")
-
+             title = "Median sale price by year") +
+        scale_fill_manual(values = colorscale) +
+        theme(legend.position = "none")
 
 averageinventorysize <-
     homesales %>% 
     group_by(listingyear) %>%
     summarize(inventory = mean(inventory)) %>%
     ggplot +
-        aes(x = listingyear, y = inventory) +
-        geom_col(alpha = alpha, fill = color) +
+        aes(x = listingyear, y = inventory, fill = listingyear) +
+        geom_col(alpha = alpha) +
         labs(x = "Year",
              y = "Average number of homes on the market",
-             title = "Average inventory")
+             title = "Average inventory size") +
+        scale_fill_manual(values = colorscale) + 
+        theme(legend.position = "none")
 
 averageinventorytime <-
     homesales %>% 
     group_by(listingyear) %>%
     summarize(inventorytime = median(inventorytime, na.rm = TRUE)) %>%
-    mutate(inventorytime = ifelse(listingyear == 2017, 0, inventorytime)) %>%
+    mutate(inventorytime = ifelse(listingyear == "2017", 0, inventorytime)) %>%
     ggplot +
-        aes(x = listingyear, y = inventorytime) +
-        geom_col(alpha = alpha, fill = color) +
+        aes(x = listingyear, y = inventorytime, fill = listingyear) +
+        geom_col(alpha = alpha) +
         labs(x = "Year",
              y = "Average Inventory Time (in months)",
              title = "Average Inventory Time by year",
              caption = paste0("Last updated on ", max_date)) + 
-        annotate("label", x = 2017, y = .5, label = "NO\nDATA")
+        annotate("label", x = "2017", y = .5, label = "NO\nDATA") +
+        scale_fill_manual(values = colorscale) +
+        theme(legend.position = "none")
 
 overview <- (homeslisted + homessold + saleprice) / (timeonmarket + averageinventorysize + averageinventorytime)
 
