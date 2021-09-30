201c07694f4c897d663ee01682ab66545ba38b11
Rob Hanssen <rob.hanssen@outlook.com>
2021-09-30 12:51:32 -0400

    3-pane dashboard

--- dashboard.r
+++ dashboard.r
@@ -45,7 +45,7 @@ homessold <-
              title = "Number of homes sold by year") +
         scale_fill_manual(values = colorscale) +
         theme(legend.position = "none") +
-        annotate("text", x = max_year, y = 2, label = "YTD")        
+        annotate("text", x = max_year, y = 2, label = "YTD")
 
 timeonmarket <-
     homesales %>%
@@ -102,15 +102,58 @@ averageinventorytime <-
              y = "Average Inventory Time (in months)",
              title = "Average Inventory Time by year",
              caption = paste0("Last updated on ", max_date)) +
-        annotate("label", x = "2017", y = .5, label = "NO\nDATA") +
+        # annotate("label", x = "2017", y = .5, label = "NO\nDATA") +
         scale_fill_manual(values = colorscale) +
         annotate("label",
                  x = "2017",
                  y = 6,
-                 label = "buyer's\nmarket\n\nseller's\nmarket") +
+                 label = "buyer's\nmarket\n\nseller's\nmarket",
+                 fill = NA) +
         geom_hline(yintercept = 6, lty = 3) +
         theme(legend.position = "none")
 
-overview <- (homeslisted + homessold + saleprice) / (timeonmarket + averageinventorysize + averageinventorytime)
+marketvalue <-
+    homesales %>%
+    filter(!is.na(saleyear)) %>%
+    group_by(saleyear) %>%
+    summarize(marketvalue = sum(amount, na.rm = TRUE)) %>%
+    ggplot +
+        aes(x = saleyear, y = marketvalue, fill = saleyear) +
+        geom_col(alpha = alpha) +
+        scale_y_continuous(breaks = 4e6 * 0:20, 
+                           labels = scales::dollar_format(scale = 1 / 1e6, suffix = "M")) +
+        labs(x = "Year",
+             y = "Total market value (in $)",
+             title = "Glen Lake total market value") +
+        scale_fill_manual(values = colorscale) +
+        annotate("text", x = max_year, y = 1e6, label = "YTD") +
+        theme(legend.position = "none")
+
+turnover <-
+    homesales %>%
+    filter(!is.na(saleyear)) %>%
+    group_by(saleyear) %>%
+    count() %>%
+    mutate(turnover = n / 482) %>%
+    ggplot +
+        aes(x = saleyear, y = turnover, fill = saleyear) +
+        geom_col(alpha = alpha) +
+        scale_y_continuous(breaks = 0.02 * 0:10,
+                           labels = scales::percent_format()) +
+        labs(x = "Year",
+             y = "Home turnover rate (in %)",
+             title = "Glen Lake home turnover rate") +
+        scale_fill_manual(values = colorscale) +
+        annotate("text", x = max_year, y = .01, label = "YTD") +
+        theme(legend.position = "none")
+
+
+
+
+# overview <- (homessold + saleprice + marketvalue) / (timeonmarket + averageinventorysize) / (homeslisted + averageinventorytime + turnover)
+
+overview <- (marketvalue + averageinventorytime + homessold) / (homeslisted + timeonmarket + averageinventorysize) / (turnover + saleprice)
+
+
 
 ggsave("dashboard.png", width = 12, plot = overview)
