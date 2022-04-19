1a577520a99edd5a605496db4c6bf9712fe4f787
Rob Hanssen <rob.hanssen@outlook.com>
2022-04-19 15:20:20 -0400

    smoothed various graphs by month

--- market-fundamentals.r
+++ market-fundamentals.r
@@ -4,6 +4,8 @@ theme_set(theme_light())
 
 load("Rdata/homesales.Rdata")
 
+source("01-config.r")
+
 homesales_filtered <-
     homesales %>%
     filter(!is.na(saledate))
@@ -14,7 +16,7 @@ datelist <-
         sort(unique(c(listingdate, saledate, today())))
     )
 
-homes_sold_last_12month <- function(tbl, date) {
+homes_sold_last_12months <- function(tbl, date) {
     date_year_ago <- date - lubridate::years(1)
     sale_data <-
         tbl %>%
@@ -24,19 +26,22 @@ homes_sold_last_12month <- function(tbl, date) {
 }
 
 sold_last_year <-
-    map_dfr(datelist, ~ homes_sold_last_12month(homesales_filtered, .x))
+    map_dfr(datelist, ~ homes_sold_last_12months(homesales_filtered, .x))
 
-monthlist <- seq(
-    floor_date(first(datelist), unit = "month"),
-    floor_date(last(datelist), unit = "month"),
-    "month"
+monthlist <- c(
+    seq(
+        floor_date(first(datelist), unit = "month"),
+        floor_date(last(datelist), unit = "month"),
+        "month"
+    ),
+    today()
 )
 
 sold_last_year_by_month <-
-    map_dfr(monthlist, ~ homes_sold_last_12month(homesales_filtered, .x))
+    map_dfr(monthlist, ~ homes_sold_last_12months(homesales_filtered, .x))
 
 
-sold_last_year %>%
+sold_last_year_by_month %>%
     filter(date >= first(datelist) + years(1)) %>%
     ggplot() +
     aes(date, homesold) +
@@ -44,10 +49,10 @@ sold_last_year %>%
     scale_x_date() +
     scale_y_continuous(limits = c(0, NA), breaks = 10 * 0:100) +
     labs(
-            x = "Date",
-            y = "Number of home sales in the last 12 months",
-            title = "Glen Lake average home sales in 12 months",
-            caption = source
+        x = "Date",
+        y = "Number of home sales in the last 12 months",
+        title = "Glen Lake average home sales in 12 months",
+        caption = source
     )
 
 ggsave("graphs/average-homesales-per-12-months.png")
@@ -74,7 +79,66 @@ inner_join(current_market_size, sold_last_year) %>%
     scale_x_date() +
     scale_y_continuous(limits = c(0, NA), breaks = 1 * 0:100) +
     geom_hline(yintercept = 6, lty = 2, color = "gray50") +
-    geom_smooth(method = "loess", se = FALSE, lty = "dashed", color = "gray50") +
+    geom_smooth(
+        method = "loess",
+        se = FALSE,
+        lty = "dashed",
+        color = "gray50"
+    ) +
+    labs(
+        x = "Date",
+        y = "Inventory rate (in months)",
+        caption = source
+    ) +
+    annotate("text", x = note_date, y = 6.5, label = "Buyer's Market") +
+    annotate("text", x = note_date, y = 5.5, label = "Seller's Market")
+
+# ggsave("graphs/average-inventory-time.png", width = 8, height = 6)
+
+
+# average residence time
+
+sold_last_year_by_month %>%
+    mutate(residencetime = 484 / homesold) %>%
+    filter(date >= first(datelist) + years(1)) %>%
+    ggplot() +
+    aes(date, residencetime) +
+    geom_line() +
+    scale_x_date() +
+    scale_y_continuous(limits = c(0, NA), breaks = 10 * 0:100) +
+    labs(
+        x = "Date",
+        y = "Average residence time (in years)",
+        title = "Glen Lake average residence time",
+        caption = source
+    )
+
+ggsave("graphs/average-residence-time.png", width = 8, height = 6)
+
+# inventory time (smoothed by month)
+
+current_market_size %>%
+    mutate(month = floor_date(date, unit = "month")) %>%
+    group_by(month) %>%
+    summarize(
+        homesonmarket = mean(homesonmarket, na.rm = TRUE),
+        .groups = "drop"
+    ) %>%
+    inner_join(sold_last_year_by_month, by = c("month" = "date")) %>%
+    mutate(market_speed = homesonmarket / homesold * 12) %>%
+    filter(month >= first(datelist) + years(1)) %>%
+    ggplot() +
+    aes(month, market_speed) +
+    geom_line(color = "gray50", alpha = .5) +
+    scale_x_date() +
+    scale_y_continuous(limits = c(0, NA), breaks = 1 * 0:100) +
+    geom_hline(yintercept = 6, lty = 2, color = "gray50") +
+    geom_smooth(
+        method = "loess",
+        se = FALSE,
+        lty = "dashed",
+        color = "gray50"
+    ) +
     labs(
         x = "Date",
         y = "Inventory rate (in months)",
